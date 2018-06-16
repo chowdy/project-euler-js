24dc7df415b3b0cb049be2408fed0ed4a0acaa13
Charlie Copprell <chowdy@gmail.com>
2018-06-15 19:54:52 -0700

    solution 14

--- app.js
+++ app.js
@@ -345,20 +345,38 @@ function problem013() {
 
 /*
  * The following iterative sequence is defined for the set of positive integers:
- *
  * n → n/2 (n is even)
  * n → 3n + 1 (n is odd)
- *
  * Using the rule above and starting with 13, we generate the following sequence:
  *
  * 13 → 40 → 20 → 10 → 5 → 16 → 8 → 4 → 2 → 1
-It can be seen that this sequence (starting at 13 and finishing at 1) contains 10 terms. Although it has not been proved yet (Collatz Problem), it is thought that all starting numbers finish at 1.
-
-Which starting number, under one million, produces the longest chain?
-
-NOTE: Once the chain starts the terms are allowed to go above one million.
+ * It can be seen that this sequence (starting at 13 and finishing at 1) contains 10 terms. Although it has not been
+ * proved yet (Collatz Problem), it is thought that all starting numbers finish at 1.
+ *
+ * Which starting number, under one million, produces the longest chain?
+ *
+ * NOTE: Once the chain starts the terms are allowed to go above one million.
  */
 function problem014() {
+    longestChain = []
+    for (number = 1; number < 1000000; number++) {//number < 1000000; number++) {
+        chain = []
+        n = number
+        while (n) {
+            chain.push(n)
+            if (n == 1) {
+                n = undefined
+            } else if (n%2 == 0) {
+                n /= 2
+            } else {
+                n = 3*n + 1
+            }
+        }
+        if (chain.length > longestChain.length) {
+            longestChain = chain
+        }
+    }
+    return longestChain[0]
 }
 
 PROBLEM = problem014
